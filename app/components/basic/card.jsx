c443892b2d4b2cc5d9084346d39fd04ad371989a
Valery Romanov <ikrakozabra@gmail.com>
2017-02-10 00:11:08 +0300

    Закрепление успехов по адаптации MDL к React, первые компоненты

--- app/components/basic/card.jsx
+++ app/components/basic/card.jsx
@@ -1,23 +1,46 @@
 import React from 'react'
 
 /**
- * Карточка Material Design Lite
+ * Константа с возможными css-классами карточек
  */
-export default class Card extends React.Component {
-    //mdl-card__title
+const cardTypes = {
+    SUBSCRIPTION_ITEM : 'SUBSCRIPTION_ITEM',
+    FEED_ITEM: 'FEED_ITEM'
+}
+
+/**
+ * Ссылки "Actions" на карточке Card
+ * ** (TODO)
+ */
+export class CardActions extends React.Component {
     render() {
         return (
-            <div class={"demo-card-wide mdl-card mdl-shadow--2dp"}>
-                <div class={"mdl-card__title"}>
-                    <h2 class={"mdl-card__title-text"}>{this.props.title}</h2>
+            <div className={"mdl-card__actions mdl-card--border"}>
+                <a className={"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"} href={this.props.url}>
+                    {this.props.text}
+                </a>
+            </div>
+        )
+    }
+}
+
+/**
+ * Карточка Material Design Lite
+ */
+export class Card extends React.Component {
+    constructor(props) {
+        super(props);
+        this.type = props.type ? cardTypes[props.type] : "";
+    }
+
+    render() {
+        return (
+            <div className={" mdl-card mdl-shadow--2dp " + this.type}>
+                <div className={"mdl-card__title"}>
+                    <h2 className={"mdl-card__title-text"}>{this.props.title}</h2>
                 </div>
                 <div class={"mdl-card__supporting-text"}>
                     {this.props.text}
-                </div>
-                <div class={"mdl-card__actions mdl-card--border"}>
-                    <a class={"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect"}>
-                        Get Started
-                    </a>
                 </div>
             </div>
         )
--- app/components/layout.jsx
+++ app/components/layout.jsx
@@ -1,13 +1,36 @@
+import '../css/mdl-icons.css'
 import '../css/material.min.css'
+import '../css/material.indigo-pink.min.css'
 import '../css/index.css'
+import '../../node_modules/material-design-lite/material.js'
 import React from 'react'
-import Card from './basic/card.jsx'
+import { Card } from './basic/card.jsx'
+
+const NAV_LINKS = [
+    { title : 'Подписки' },
+    { title : 'Что-то ещё' },
+    { title : 'И ещё!' },
+]
 
 export default class Layout extends React.Component {
     render() {
+        let navLinks = NAV_LINKS.map(link => <a className={'mdl-navigation__link'} href={'#'}>{link.title}</a>)
         return (
-            <div id={"app-layout"}>
-                <Card title={'Oops'} text={'AHHHHHHAAHAHHAHH! I fuckin did it'}/>
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+                <header className="mdl-layout__header">
+                    <div className="mdl-layout__header-row">
+                        <span className="mdl-layout-title">RSS-ленты</span>
+                    </div>
+                </header>
+                <div className="mdl-layout__drawer">
+                    <span className="mdl-layout-title">Действия</span>
+                    <nav className="mdl-navigation">
+                        {navLinks}
+                    </nav>
+                </div>
+                <main className="mdl-layout__content">
+                    <div className="page-content">Когда-нибудь здесь действительно будет какой-то контент. Ну, возможно...</div>
+                </main>
             </div>
         )
     }
