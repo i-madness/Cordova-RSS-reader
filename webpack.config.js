111b36242f751ca4a9d61b561d9958f954c579b1
Valery Romanov <ikrakozabra@gmail.com>
2017-02-16 21:07:03 +0300

    :ghost: Проект адаптирован под Cordova (o rly?)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     devtool: debug ? "inline-sourcemap" : null,
     entry: "./application.js",
     output: {
-        path: __dirname,/* + "/js",*/
+        path: __dirname + "/js",
         filename: "bundle.min.js"
     },
     plugins: debug ? [] : [
