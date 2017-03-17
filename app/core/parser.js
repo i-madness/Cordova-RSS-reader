70808ad7e9742805635e982de28b1e021896c1c9
Valery Romanov <ikrakozabra@gmail.com>
2017-03-17 20:15:12 +0300

    :bug: Исправлена неправильная сортировка при апдейте ленты

--- app/core/parser.js
+++ app/core/parser.js
@@ -42,38 +42,37 @@ export const FeedParser = {
      * @param urls {Array<String>} список из URL каналов, для которых надо получить элементы ленты
      * @returns {Promise}
      */
-    parseSubscription: (channels) =>
-        dispatch => {
-            dispatch({ type: FeedActionTypes.ENTRIES_LOADING })
-            feedPromise = Promise.all(channels.map(channel => fetch(channel.url)))
-                .then(responses => Promise.all(responses.map(resp => resp.text())))
-                .then(texts => {
-                    let commonResult = []
-                    let txtIndex = 0
-                    for (let txt of texts) {
-                        try {
-                            let doc = PARSER_INSTANCE.parseFromString(txt, "text/xml")
-                            let items = Array.from(doc.querySelectorAll('item'))
-                            let result = items.map(item => {
-                                let title = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'title').innerHTML)
-                                let link = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'link').innerHTML)
-                                let timestamp = moment(Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'pubDate').innerHTML), MOMENT_FORMAT).unix()
-                                let description = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'description').innerHTML)
-                                return new FeedItem(title, description, timestamp, link, channels[txtIndex]);
-                            }).filter(item => !!item)
-                            commonResult = [...commonResult, ...result]
-                        } catch (err) {
-                            return Promise.reject('text:\n' + txt);
-                        }
-                        txtIndex++
+    parseSubscription: (channels) => dispatch => {
+        dispatch({ type: FeedActionTypes.ENTRIES_LOADING })
+        feedPromise = Promise.all(channels.map(channel => fetch(channel.url)))
+            .then(responses => Promise.all(responses.map(resp => resp.text())))
+            .then(texts => {
+                let commonResult = []
+                let txtIndex = 0
+                for (let txt of texts) {
+                    try {
+                        let doc = PARSER_INSTANCE.parseFromString(txt, "text/xml")
+                        let items = Array.from(doc.querySelectorAll('item'))
+                        let result = items.map(item => {
+                            let title = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'title').innerHTML)
+                            let link = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'link').innerHTML)
+                            let timestamp = moment(Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'pubDate').innerHTML), MOMENT_FORMAT).unix()
+                            let description = Utils.cleanupContent(_.find(item.childNodes, child => child.nodeName === 'description').innerHTML)
+                            return new FeedItem(title, description, timestamp, link, channels[txtIndex]);
+                        }).filter(item => !!item)
+                        commonResult = [...commonResult, ...result]
+                    } catch (err) {
+                        return Promise.reject('text:\n' + txt);
                     }
-                    return Promise.resolve(commonResult)
-                })
+                    txtIndex++
+                }
+                return Promise.resolve(commonResult)
+            })
 
-                feedPromise
-                    .then(result => dispatch({ type: FeedActionTypes.ENTRIES_LOADING_SUCCESS, payload: result }))
-                    .catch(error => dispatch({ type: FeedActionTypes.ENTRIES_LOADING_FAILURE, payload: error }))
-        },
+        feedPromise
+            .then(result => dispatch({ type: FeedActionTypes.ENTRIES_LOADING_SUCCESS, payload: result }))
+            .catch(error => dispatch({ type: FeedActionTypes.ENTRIES_LOADING_FAILURE, payload: error }))
+    },
 
     /**
      * Метод добавления новой RSS-ленты, основанный на fetch. Делает проверку ленты на валидность.
@@ -86,30 +85,29 @@ export const FeedParser = {
      * *** 'http://resource.com/', добавляя к ним 'rss.xml' (типа традиционный URL для лент новостных сайтов)
      * 
      */
-    addRssFeed: (url) =>
-        dispatch => {
-            if (!url) {
-                return dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING_FAILURE, payload: 'Поле "URL канала" не может быть пустым' })
-            }
-            dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING })
-            fetch(url)
-                .then(response => response.text())
-                .then(txt => {
-                    if (!txt.includes('<?xml') && !txt.includes('<rss version=')) {
-                        return Promise.reject('Запрашиваемый ресурс "' + url + '" не является RSS-лентой')
-                    }
-                    let doc = PARSER_INSTANCE.parseFromString(txt, "text/xml");
-                    return Promise.resolve({
-                        title: _.find(Array.from(doc.querySelector('channel').childNodes), node => node.nodeName === 'title').innerHTML,
-                        description: Utils.cleanupContent(_.find(Array.from(doc.querySelector('channel').childNodes), node => node.nodeName === 'description').innerHTML),
-                        url: url,
-                        id: _.uniqueId('ch')
-                    })
+    addRssFeed: (url) => dispatch => {
+        if (!url) {
+            return dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING_FAILURE, payload: 'Поле "URL канала" не может быть пустым' })
+        }
+        dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING })
+        fetch(url)
+            .then(response => response.text())
+            .then(txt => {
+                if (!txt.includes('<?xml') && !txt.includes('<rss version=')) {
+                    return Promise.reject('Запрашиваемый ресурс "' + url + '" не является RSS-лентой')
+                }
+                let doc = PARSER_INSTANCE.parseFromString(txt, "text/xml");
+                return Promise.resolve({
+                    title: _.find(Array.from(doc.querySelector('channel').childNodes), node => node.nodeName === 'title').innerHTML,
+                    description: Utils.cleanupContent(_.find(Array.from(doc.querySelector('channel').childNodes), node => node.nodeName === 'description').innerHTML),
+                    url: url,
+                    id: _.uniqueId('ch')
                 })
-                .then(result => {
-                    dispatch({ type: SubActionTypes.ADD_SUBSCRIPTION, payload: result })
-                    Scheduler.update()
-                })
-                .catch(error => dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING_FAILURE, payload: error }))
-        },
+            })
+            .then(result => {
+                dispatch({ type: SubActionTypes.ADD_SUBSCRIPTION, payload: result })
+                Scheduler.update()
+            })
+            .catch(error => dispatch({ type: SubActionTypes.SUBSCRIPTIONS_LOADING_FAILURE, payload: error }))
+    },
 }
